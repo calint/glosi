a2d513065c933f749f73a0578c919aad2a702704
calint <calin.tenitchi@gmail.com>
2024-07-25 13:39:26 +0200

    .

--- src/engine/engine.hpp
+++ src/engine/engine.hpp
@@ -21,6 +21,7 @@
 #include <format>
 #include <fstream>
 #define GLM_FORCE_INTRINSICS
+#define GLM_FORCE_INLINE
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
