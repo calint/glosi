ddb1667c8893293399e40d103e1c0d1c73c2f488
calint <calin.tenitchi@gmail.com>
2025-09-18 16:29:30 +0700

    .

--- make.sh
+++ make.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# dependencies (in ubuntu 24.04 installation):
-# * clang++ version 18.1.3
+# dependencies in ubuntu 24.04 installation:
+# * clang++ 18.1.3
 # * libglm-dev/noble,noble,now 0.9.9.8+ds-7
 # * libtbb-dev/noble,now 2021.11.0-2ubuntu2
 # * libsdl2-dev/noble,now 2.30.0+dfsg-1build3
@@ -8,6 +8,17 @@
 # * libsdl2-image-dev/noble,now 2.8.2+dfsg-1build2
 # * libsdl2-ttf-dev/noble,now 2.22.0+dfsg-1
 # * libgl-dev/noble,now 1.7.0-1build1
+#
+# dependencies in arch linux installation:
+# * clang++ 20.1.8
+# * mesa 1:25.2.3-1
+# * glm 1.0.1
+# * sdl3 3.2.22-1
+# * sdl2-compat 2.32.56-2
+# * sdl2_ttf 2.24.0-2
+# * sdl2_image 2.8.8-1
+# * intel-oneapi-tbb 2021.12.0-2
+
 set -e
 cd $(dirname "$0")
 
